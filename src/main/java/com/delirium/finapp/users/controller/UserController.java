3f1bc4994b04978b0eefdf9f04261336be3beb1d
style <style.daniel@gmail.com>
2015-12-08 21:06:52 +0200

    Updated groupUsers service

--- src/main/java/com/delirium/finapp/users/controller/UserController.java
+++ src/main/java/com/delirium/finapp/users/controller/UserController.java
@@ -38,15 +38,17 @@ public class UserController {
     }
 
     @PreAuthorize("isAuthenticated()")
-    @RequestMapping(value = "/users", params = {
-        "groupId"}, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/users/{groupId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity<List<User>> findGroupUsers(@RequestParam("groupId") Long groupId) {
+    public ResponseEntity<List<User>> findGroupUsers(@PathVariable("groupId") Long groupId) {
         Group group = groupService.findById(groupId);
         if (group == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         List<User> users = group.getUsers();
+        if (!users.contains(userService.findCurrentUser())) {
+            return new ResponseEntity<List<User>>(HttpStatus.UNAUTHORIZED);
+        }
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
