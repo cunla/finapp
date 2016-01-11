7a9de5625f944c47096076ff02f2dc1bd1e919ba
unknown <morand3@isenmorand3l1c.corp.emc.com>
2016-01-11 14:11:35 +0200

    add category form

--- src/main/webapp/www/js/services.js
+++ src/main/webapp/www/js/services.js
@@ -28,45 +28,7 @@
                         data: {email: email, password: password}
                     });
                 },
-                fbLogin: function () {
-                    return $q(function (resolve, reject) {
-                        FB.login(function (response) {
-                            if (response.authResponse) {
-                                getUserInfo();
-                            } else {
-                                console.log('User cancelled login or did not fully authorize.');
-                                reject('User cancelled login or did not fully authorize.');
-                            }
-                        }, {scope: 'email'});
-
-                        function getUserInfo() {
-                            // get basic info
-                            FB.api('/me',
-                                {fields: "id,about,age_range,picture,bio,birthday,context,email,first_name,gender,hometown,link,location,middle_name,name,timezone,website,work"},
-                                function (response) {
-                                    // get profile picture
-                                    FB.api('/me/picture?type=normal', function (picResponse) {
-                                        console.log('Facebook Login RESPONSE: ' + picResponse.data.url);
-                                        response.imageUrl = picResponse.data.url;
-                                        // After posting user data to server successfully store user data locally
-                                        var user = {};
-                                        user.name = response.name;
-                                        user.email = response.email;
-                                        if (response.gender) {
-                                            response.gender.toString().toLowerCase() === 'male' ? user.gender = 'M' : user.gender = 'F';
-                                        } else {
-                                            user.gender = '';
-                                        }
-                                        user.avatar = picResponse.data.url;
-                                        $http.post(basicUrl + '/facebookuser?accountId=' + response.id, user, {headers: {"Content-Type": "application/json"}})
-                                            .then(function (data) {
-                                                resolve(data.data);
-                                            })
-                                    });
-                                });
-                        }
-                    });
-                },
+                fbLogin: fbLogin,
                 logout: function () {
                     return $http.post(basicUrl + '/logout-action');
                 },
@@ -116,10 +78,52 @@
                 },
                 getAccountsReport: function (group) {
                     return $http.get('data/accounts.json');
+                },
+                createCategory: function (cat) {
+                    console.log("Creating " + cat);
+                    return;
                 }
             };
         }]);
+    function fbLogin() {
+        return $q(function (resolve, reject) {
+            FB.login(function (response) {
+                if (response.authResponse) {
+                    getUserInfo();
+                } else {
+                    console.log('User cancelled login or did not fully authorize.');
+                    reject('User cancelled login or did not fully authorize.');
+                }
+            }, {scope: 'email'});
 
+            function getUserInfo() {
+                // get basic info
+                FB.api('/me',
+                    {fields: "id,about,age_range,picture,bio,birthday,context,email,first_name,gender,hometown,link,location,middle_name,name,timezone,website,work"},
+                    function (response) {
+                        // get profile picture
+                        FB.api('/me/picture?type=normal', function (picResponse) {
+                            console.log('Facebook Login RESPONSE: ' + picResponse.data.url);
+                            response.imageUrl = picResponse.data.url;
+                            // After posting user data to server successfully store user data locally
+                            var user = {};
+                            user.name = response.name;
+                            user.email = response.email;
+                            if (response.gender) {
+                                response.gender.toString().toLowerCase() === 'male' ? user.gender = 'M' : user.gender = 'F';
+                            } else {
+                                user.gender = '';
+                            }
+                            user.avatar = picResponse.data.url;
+                            $http.post(basicUrl + '/facebookuser?accountId=' + response.id, user, {headers: {"Content-Type": "application/json"}})
+                                .then(function (data) {
+                                    resolve(data.data);
+                                })
+                        });
+                    });
+            }
+        });
+    }
     function datePickerFunc(title, date) {
         var d = {
             titleLabel: title,  //Optional
--- src/main/webapp/www/partials/category/category.ctrl.js
+++ src/main/webapp/www/partials/category/category.ctrl.js
@@ -3,13 +3,15 @@
         .controller('catCtrl', ['$scope', 'fin', '$stateParams', '$rootScope', '$ionicPopup', 'datePicker', catCtrl])
     function catCtrl($scope, fin, $stateParams, $rootScope, $ionicPopup, datePicker) {
         var groupId = $stateParams.groupId;
-        //$scope.periodType = "Custom";
-
+        $scope.colors = colors();
+        $scope.icons = icons();
         $scope.startDateObject = datePicker.datePickerObject("Start date", datePicker.beginningOfMonth());
         $scope.endDateObject = datePicker.datePickerObject("End date", datePicker.endOfMonth());
         if ($rootScope.user) {
-            $scope.startDateObject.inputDate = $rootScope.user.defaultStart;
-            $scope.endDateObject.inputDate = $rootScope.user.defaultEnd;
+            if ($rootScope.user.defaultStart)
+                $scope.startDateObject.inputDate = $rootScope.user.defaultStart;
+            if ($rootScope.user.defaultEnd)
+                $scope.endDateObject.inputDate = $rootScope.user.defaultEnd;
         }
         $scope.doRefresh = function () {
             fin.getCategoryReport(groupId, $scope.startDateObject.inputDate, $scope.endDateObject.inputDate)
@@ -29,7 +31,38 @@
                     $scope.$broadcast('scroll.refreshComplete');
                 })
         }
+        $scope.toggleAddForm = function () {
+            $scope.showAddForm = !$scope.showAddForm;
+        }
+        $scope.ncat = {};
+        $scope.createCategory = function (cat) {
+            $scope.showAddForm = false;
+            fin.createCategory(cat).then(function (res) {
+
+            });
+        }
 
         $scope.doRefresh();
     };
+    function colors() {
+        return ['stable', 'positive', 'calm', 'balanced', 'energized', 'assertive', 'royal', 'dark'];
+    };
+    function icons() {
+        return ['ion-bag',
+            'ion-ios-cart',
+            'ion-home',
+            'ion-earth',
+            'ion-model-s',
+            'ion-card',
+            'ion-social-bitcoin-outline',
+            'ion-cash',
+            'ion-social-usd',
+            'ion-social-facebook',
+            'ion-social-euro',
+            'ion-social-tux',
+            'ion-social-apple',
+            'ion-social-android',
+            'ion-record'
+        ];
+    }
 })();
